4c4d23c8cc65dbadc14b018c44d922d409064274
Vishnu <dummy.vishnu66@gmail.com>
2024-07-26 17:29:58 +0530

    added detail page

--- src/component/PostDetail/PostDetail.jsx
+++ src/component/PostDetail/PostDetail.jsx
@@ -2,8 +2,16 @@
 const PostDetail = () =>  {
     return (
         <section className="detail-page">
-            <div>
-                <h1>Title of the image</h1>
+            <div className="card-detail">
+                <h1 className="mb-4 text-2xl font-semibold ">Why the security engineers loves working in fosec</h1>
+                <div>
+                    <img src="https://media.istockphoto.com/id/1488944909/photo/environment-concept-green-glass-globe-with-a-tree-in-the-forest-with-sunlight-sustainability.webp?b=1&s=170667a&w=0&k=20&c=f89zMjUr7FldJzgS0p8mJyuTxBWgvhv0kBQL7fcSnjs=" alt="detail page" />
+                </div>
+                <div className="flex justify-between mt-4">
+                    <button className="px-4 py-2 bg-orange-500 rounded-md"><i className="fa-solid fa-pen-to-square"></i></button>
+                    <button className="px-4 py-2 bg-orange-500 rounded-md"><i className="fa-solid fa-delete-left"></i></button>
+                </div>
+                <p className="mt-8 text-justify">Why the In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is availableIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is availableIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available security engineers loves In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available working in fosec Why the security engineers loves working in fosecWhy the security engineers loves working in fosecWhy the security engineers loves working in fosecWhy the security engineers loves working in fosecWhy the security engineers loves working in fosec</p>
             </div>
         </section>
     )
