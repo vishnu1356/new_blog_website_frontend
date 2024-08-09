041e7bbcdb1f950c54c7a2ecfd0cec1938e3a8d4
Vishnu <dummy.vishnu66@gmail.com>
2024-08-09 14:03:11 +0530

    added sidebar disappear

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import Tooltip from "../Tooltip/Tooltip";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import UserContext from "../../contex/user";
 import toast from "react-hot-toast";
 
 const Sidebarcomp = (props) => {
-    console.log("props from sidebarcomp", props);
-    const {userData, setUserData} = useContext(UserContext)
-    function handleLogout () {
+    // console.log("props from sidebarcomp", props);
+    const { userData, setUserData } = useContext(UserContext)
+    function handleLogout() {
         props.handleSidebar()
         setUserData({
             username: "",
@@ -17,34 +17,38 @@ const Sidebarcomp = (props) => {
         toast.success("Logged out successfully")
     }
 
+    function handleMouseLeave () {
+        console.log("MouseLeave clicked")
+        props.handleSidebar();
+    }
     return (
-                    <div className=" w-full h-[45vh] sm:w-[200px] px-4 py-4 bg-white  backdrop-blur-md top-18 left-0 z-50 fixed">
+        <div onMouseLeave={handleMouseLeave} className=" w-full h-[45vh] sm:w-[200px] px-4 py-4 bg-white  backdrop-blur-md top-18 left-0 z-50 fixed">
             <div className="flex justify-center flex-col gap-8">
-                <div className="flex gap-4 items-center"  onClick={() => props.handleSidebar()}>
+                <div className="flex gap-4 items-center" onClick={() => props.handleSidebar()}>
                     <i className="fa-solid fa-user  text-xl text-orange-500"></i>
                     {/* <p className="text-white text-xl cursor-pointer hover:bg-orange-500 px-2 py-1 rounded-md">User</p> */}
-                    <Tooltip username={userData.username} email={userData.email} usertype={userData.usertype}/>
-                </div> 
-                
+                    <Tooltip username={userData.username} email={userData.email} usertype={userData.usertype} />
+                </div>
+
                 {
                     (userData.username) ? <div className="flex gap-4 items-center">
-                    <i className="fa-solid fa-plus  text-2xl text-orange-500"></i>
-                    <Link to={"/post/create"}  onClick={() => props.handleSidebar()} className="text-black text-xl cursor-pointer hover:text-orange-500 font-semibold px-2 py-1 rounded-md">Create Post</Link>
-                </div> : ""
+                        <i className="fa-solid fa-plus  text-2xl text-orange-500"></i>
+                        <Link to={"/post/create"} onClick={() => props.handleSidebar()} className="text-black text-xl cursor-pointer hover:text-orange-500 font-semibold px-2 py-1 rounded-md">Create Post</Link>
+                    </div> : ""
                 }
                 <div className="flex gap-4 items-center">
                     <i className="fa-solid fa-house  text-xl text-orange-500"></i>
                     <Link to={"/"} onClick={() => props.handleSidebar()} className="text-black text-xl cursor-pointer  hover:text-orange-500 font-semibold px-2 py-1 rounded-md">All Post</Link>
-                </div> 
+                </div>
                 {
                     (userData.username) ? <div className="flex gap-4 items-center">
-                    <i className="fa-solid fa-right-from-bracket text-xl text-orange-500"></i>
-                     <p onClick={handleLogout}  className="text-black text-xl cursor-pointer  hover:text-orange-500 font-semibold px-2 py-1 rounded-md">Log Out</p>
-                 </div> : ""
+                        <i className="fa-solid fa-right-from-bracket text-xl text-orange-500"></i>
+                        <p onClick={handleLogout} className="text-black text-xl cursor-pointer  hover:text-orange-500 font-semibold px-2 py-1 rounded-md">Log Out</p>
+                    </div> : ""
                 }
 
             </div>
-            
+
         </div>
 
     )
