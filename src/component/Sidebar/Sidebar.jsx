53aa77d6375ba37aa1b4288a07c935e0f89f6e36
Vishnu <dummy.vishnu66@gmail.com>
2024-08-09 13:01:57 +0530

    added styling

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebarcomp = (props) => {
     }
 
     return (
-                    <div className=" w-full h-[45vh] sm:w-[200px] px-4 py-4 bg-gray-300  backdrop-blur-md top-18 left-0 z-50 fixed">
+                    <div className=" w-full h-[45vh] sm:w-[200px] px-4 py-4 bg-white  backdrop-blur-md top-18 left-0 z-50 fixed">
             <div className="flex justify-center flex-col gap-8">
                 <div className="flex gap-4 items-center"  onClick={() => props.handleSidebar()}>
                     <i className="fa-solid fa-user  text-xl text-orange-500"></i>
@@ -29,17 +29,17 @@ const Sidebarcomp = (props) => {
                 {
                     (userData.username) ? <div className="flex gap-4 items-center">
                     <i className="fa-solid fa-plus  text-2xl text-orange-500"></i>
-                    <Link to={"/post/create"}  onClick={() => props.handleSidebar()} className="text-white text-xl cursor-pointer hover:bg-[#545454] px-2 py-1 rounded-md">Create Post</Link>
+                    <Link to={"/post/create"}  onClick={() => props.handleSidebar()} className="text-black text-xl cursor-pointer hover:text-orange-500 font-semibold px-2 py-1 rounded-md">Create Post</Link>
                 </div> : ""
                 }
                 <div className="flex gap-4 items-center">
                     <i className="fa-solid fa-house  text-xl text-orange-500"></i>
-                    <Link to={"/"} onClick={() => props.handleSidebar()} className="text-white text-xl cursor-pointer hover:bg-[#545454] px-2 py-1 rounded-md">All Post</Link>
+                    <Link to={"/"} onClick={() => props.handleSidebar()} className="text-black text-xl cursor-pointer  hover:text-orange-500 font-semibold px-2 py-1 rounded-md">All Post</Link>
                 </div> 
                 {
                     (userData.username) ? <div className="flex gap-4 items-center">
                     <i className="fa-solid fa-right-from-bracket text-xl text-orange-500"></i>
-                     <p onClick={handleLogout}  className="text-white text-xl cursor-pointer hover:bg-[#545454] px-2 py-1 rounded-md">Log Out</p>
+                     <p onClick={handleLogout}  className="text-black text-xl cursor-pointer  hover:text-orange-500 font-semibold px-2 py-1 rounded-md">Log Out</p>
                  </div> : ""
                 }
 
