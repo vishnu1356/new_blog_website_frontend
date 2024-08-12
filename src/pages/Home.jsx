5db00cac6c47268ff20161ad0761fbe3125231a2
Vishnu <dummy.vishnu66@gmail.com>
2024-08-12 10:09:14 +0530

    added faq' section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,11 +16,12 @@ const Home = () => {
     const {inputData, setInputData} =  useContext(InputSearch);
 
     const faqdata = [
-        { question: "How much is your fee", answer: "We are always here to help you with any questions you may have about our blog. Feel free to ask your questions below." },
-        { question: "How much is your fee", answer: "We are always here to help you with any questions you may have about our blog. Feel free to ask your questions below." },
-        { question: "How much is your fee", answer: "We are always here to help you with any questions you may have about our blog. Feel free to ask your questions below." },
-        { question: "How much is your fee", answer: "We are always here to help you with any questions you may have about our blog. Feel free to ask your questions below." },
-        { question: "How much is your fee", answer: "We are always here to help you with any questions you may have about our blog. Feel free to ask your questions below." },
+        { question: "What is JavaScript", answer: "JavaScript is a programming language used to make web pages interactive. It runs in your browser, allowing things like animations, form validation, and dynamic content updates." },
+        { question: "What is closure in JavaScript", answer: "Closures in JavaScript are functions that remember and have access to variables from their outer scope, even after that outer function has finished executing. This allows the inner function to access and manipulate those variables whenever it's invoked." },
+        { question: "Promise in JavaScript", answer: "A Promise in JavaScript is an object that represents the eventual completion or failure of an asynchronous operation. It allows you to attach callbacks to handle the result once the operation is finished. Promises help manage asynchronous code in a more organized and readable way." },
+        { question: "What is React", answer: "React is a JavaScript library for building user interfaces, primarily for single-page applications. It allows developers to create reusable UI components and efficiently update and render them based on data changes. React focuses on building components that manage their own state and can be composed to create complex UIs." },
+        { question: "Why React is so powerful", answer: "The React reconciliation process compares the current virtual DOM with the previous one to identify changes. It efficiently updates only the parts of the actual DOM that have changed, ensuring minimal performance impact by avoiding unnecessary updates. This process makes React's UI updates fast and efficient." },
+        { question: "What is higher order component", answer: "Higher-Order Components (HOCs) in React are functions that enhance a component by wrapping it with additional functionality, allowing code reuse without modifying the original component." },
     ]
 
     async function fetchBlogs() {
