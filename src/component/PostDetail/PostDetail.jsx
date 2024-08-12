94bccf0ff49a9b10537c6c8a10d863d7cbedc804
Vishnu <dummy.vishnu66@gmail.com>
2024-08-12 09:15:15 +0530

    added detail page

--- src/component/PostDetail/PostDetail.jsx
+++ src/component/PostDetail/PostDetail.jsx
@@ -41,22 +41,36 @@ const PostDetail = () =>  {
             <section className="detail-page">
                 <div className="card-detail">
                     <h1 className="mb-4 text-3xl font-semibold ">{singleBlog.title}...</h1>
-                    <div>
+                    <div className="mx-12 bg-blue-600">
                         <img src={`${import.meta.env.VITE_IMG_URL}/${id}`} alt="detail page" />
                     </div>
+                    <div>
+                    {/* <div className="mt-4 mb-4 flex items-center">
+                        <p className="font-bold text-orange-500">Category:  <span className="bg-black font-normal text-white px-2 py-1 rounded-md">Health</span></p>
+                    </div> */}
+                    <div className="flex gap-4 mt-4">
+                        <p className="font-bold text-orange-500">Tags:</p>
+                        {
+                            allTags.map((tag, idx) => <p key={idx} className="bg-black text-white px-2 py-1 rounded-md">{tag}</p>)
+                        }
+                        {/* <span className="bg-black text-white px-2 py-1 rounded-md">one</span>
+                        <span className="bg-black text-white px-2 py-1 rounded-md">one</span>
+                        <span className="bg-black text-white px-2 py-1 rounded-md">one</span> */}
+                    </div>
+                </div>
                     <div className="flex justify-between mt-4">
-                        <button className="px-4 py-2 bg-orange-500 rounded-md"><i className="fa-solid fa-pen-to-square"></i></button>
+                        <p className="font-bold text-orange-500">Category:  <span className="bg-black font-normal text-white px-2 py-1 rounded-md">Health</span></p>
+
                         <button onClick={handleDeletePost} className="px-4 py-2 bg-orange-500 rounded-md"><i className="fa-solid fa-delete-left"></i></button>
+
                     </div>
                     <p className="mt-8 text-justify">{singleBlog.description}</p>
                 </div>
+
             </section>
 
-            <div className="flex flex-col me-8">
-                {/* <div className=" mb-16">
-                    <p className="text-2xl font-bold mb-4">Category Type </p>
-                    <p className="text-xl font-sm ">Money</p>
-                </div> */}
+            {/* <div className="flex flex-col me-8">
+
                 <div className="h-[200px] mb-8 bg-slate-300 p-4 rounded-lg">
                         <h1 className="text-3xl font-bold mb-4 text-center">Category Type</h1>
                     <div className="flex gap-4 text-xl ">
@@ -74,7 +88,7 @@ const PostDetail = () =>  {
                     
                 </div>
                 </div>
-            </div>
+            </div> */}
             
         </div>
     )
